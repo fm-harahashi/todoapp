4052b9ba6b28a747bebb5c2744c1a59e2e1eb216
naoya <mogamimaly3603@gmail.com>
2020-06-29 13:33:49 +0900

    チュートリアル5　フォルダの新規作成機能の実装のためにweb.phpにRouteを追加しました。

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,7 +18,7 @@ class TaskController extends Controller
 
         // 選ばれたフォルダに紐づくタスクを取得する
         $tasks = $current_folder->getTasksList()->get();
-        
+
         return view('tasks/index', [
             'folders' => $folders,
             'current_folder_id' => $current_folder->id,
